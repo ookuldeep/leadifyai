b78eac89362d9106fdda0d9f3f495d8b46d0279e
kuldeep chandrakar <kuldeepchandrakar28042001@gmail.com>
2025-08-06 00:04:55 +0530

    removed node-fech

--- backend/routes/leads.js
+++ backend/routes/leads.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
+
 const { Pool } = require('pg'); // Assuming you're using pg for your database
 
 // Database connection pool
