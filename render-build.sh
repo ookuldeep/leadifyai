38a845322a9454293c3e8d5a170eda5b8992e93d
kuldeep chandrakar <kuldeepchandrakar28042001@gmail.com>
2025-08-06 22:11:08 +0530

    Fis

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install all dependencies
+npm install
+
+# Ensure node-fetch is installed (important)
+npm install node-fetch@2.6.1
+
+# Start the backend server
+npm start
