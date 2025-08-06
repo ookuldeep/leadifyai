1b6e994bf60842aa49cb9d1108768147e4d9d95f
kuldeep chandrakar <kuldeepchandrakar28042001@gmail.com>
2025-08-06 21:38:46 +0530

    Fix: add node-fetch to dependencies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+require('node-fetch'); // if you're using it
+
 
 const app = express(); // ✅ Define app FIRST
 const PORT = process.env.PORT || 5000;
